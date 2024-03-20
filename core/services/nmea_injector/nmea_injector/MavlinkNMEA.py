93f6a12dc7850d218256cbe04c28580019d6e440
The-JRC <jcampbell98@outlook.com>
2024-03-20 12:37:43 +0930

    reformatting

--- core/services/nmea_injector/nmea_injector/MavlinkNMEA.py
+++ core/services/nmea_injector/nmea_injector/MavlinkNMEA.py
@@ -6,7 +6,6 @@ from pydantic import BaseModel
 
 from nmea_injector.exceptions import UnsupportedSentenceType
 
-
 class MAVLINK_GPS_FIX_TYPE(IntEnum):
     GPS_FIX_TYPE_NO_GPS = 0
     GPS_FIX_TYPE_NO_FIX = 1
@@ -18,7 +17,6 @@ class MAVLINK_GPS_FIX_TYPE(IntEnum):
     GPS_FIX_TYPE_STATIC = 7
     GPS_FIX_TYPE_PPP = 8
 
-
 class MAVLINK_GPS_INPUT_IGNORE_FLAG(IntFlag):
     GPS_INPUT_IGNORE_FLAG_ALT = 1
     GPS_INPUT_IGNORE_FLAG_HDOP = 2
@@ -29,10 +27,8 @@ class MAVLINK_GPS_INPUT_IGNORE_FLAG(IntFlag):
     GPS_INPUT_IGNORE_FLAG_HORIZONTAL_ACCURACY = 64
     GPS_INPUT_IGNORE_FLAG_VERTICAL_ACCURACY = 128
 
-
 class Mavlink2RestBitEnum(BaseModel):
     bits: int
-
 
 nmea_ignore_flags = (
     MAVLINK_GPS_INPUT_IGNORE_FLAG.GPS_INPUT_IGNORE_FLAG_SPEED_ACCURACY
@@ -40,10 +36,8 @@ nmea_ignore_flags = (
     | MAVLINK_GPS_INPUT_IGNORE_FLAG.GPS_INPUT_IGNORE_FLAG_VEL_HORIZ
 )
 
-
 class MavlinkGpsInput(BaseModel):
     """Mavlink's package specification to input GPS data into stream (GPS_INPUT)."""
-
     type: str = "GPS_INPUT"
     time_usec: Optional[int] = 0
     gps_id: Optional[int] = 0
@@ -67,7 +61,6 @@ class MavlinkGpsInput(BaseModel):
     satellites_visible: Optional[int] = 0
     yaw: Optional[int] = 0
 
-
 def map_gps_qual_to_mavlink_gps_fix_type(gps_qual_value: int) -> int:
     """Map pynmea2 GPS quality value to mavlink GPS fix types."""
     gps_qual_mapping = {
@@ -82,7 +75,6 @@ def map_gps_qual_to_mavlink_gps_fix_type(gps_qual_value: int) -> int:
     return gps_qual_mapping.get(
         gps_qual_value, MAVLINK_GPS_FIX_TYPE.GPS_FIX_TYPE_NO_GPS.value
     )
-
 
 def parse_mavlink_from_sentence(msg: pynmea2.NMEASentence) -> MavlinkGpsInput:
     data: Dict[str, Any] = {}
